6b8ac78edf5b7ae2af4e4c476827e963f9daf7b1
Travis Whitaker <pi.boy.travis@gmail.com>
2013-04-18 18:16:11 -0400

    Better argument messages.

--- src/main.c
+++ src/main.c
@@ -21,12 +21,17 @@ int main(int argc, char *argv[])
 {
 	switch(argc)
 	{
-		case 2:
-			break;
-		default:
-			printf("Not necessarily beautiful, but mutated...\n");
-			return 0;
-			break;
+	case 2:
+		break;
+	case 1:
+	case 0:
+		printf("Too few arguments...\n");
+		return 0;
+		break;
+	default:
+		printf("Too many arguments...\n");
+		return 0;
+		break;
 	}
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
@@ -35,14 +40,14 @@ int main(int argc, char *argv[])
 
 	if((addrstatus = getaddrinfo(NULL,"9999",&hints,&addrInfo)) != 0)
 	{
-		printf("Couldn't run getaddrinfo ya bimbooo...\n");
+		printf("She's out of sync...\n");
 		return 0;
 	}
 
 	tube = socket(addrInfo->ai_family, addrInfo->ai_socktype, addrInfo->ai_protocol);
 	if(tube == -1)
 	{
-		printf("Failed to build tube.\n");
+		printf("The Internet is not a truck, but rather a series of tubes...\n");
 		return 0;
 	}
 
